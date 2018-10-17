50a2fb651dc740d38c9d2dcb3c0dafeb5edaae20
강수진 <rkdthd1234@naver.com>
2018-10-17 20:21:19 +0900

    create project

---
+++ Ppulma/Ppulma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ppulma
+//
+//  Created by 강수진 on 2018. 10. 17..
+//  Copyright © 2018년 강수진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
